b60928935273811f0884e357beeefb6d588a4f74
BochkovPavel <bochkovpv@mail.ru>
2020-01-21 23:45:17 +0300

    Add class Calculator and enum RomNum

--- jm_task_calculator/src/Solution.java
+++ jm_task_calculator/src/Solution.java
@@ -6,23 +6,39 @@ import java.util.regex.Pattern;
 public class Solution {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Calculator calc = new Calculator();
         String str = "";
-        String strList[];
-        int res;
         while (true){
+
             try {
                 str = reader.readLine();
-                if (Pattern.matches("^\\d+\\s[\\+\\-\\/\\*]{1}\\s\\d+", str)){
-                    strList = str.split("\\s");
-                    
-                    System.out.println(strList[0] + strList[1] + strList[2]);
-                } else break;
+                if (str.equals("exit")){
+                    System.out.println("Введена команда \"exit\"!");
+                    break;
+                //}
+                //if (Pattern.matches("^\\D+\\s// [\\+\\-\\/\\*]{1}\\s\\D+", str)){
+                   // strList = str.split("\\s");
+
+                  //  System.out.println(romanToArabic(strList[0]) + strList[1] + romanToArabic(strList[2])
+                  //          + arabicToRoman(romanToArabic(strList[0])) + arabicToRoman(romanToArabic(strList[0])));
+                } else  if (Pattern.matches("^\\d+\\s[\\+\\-\\/\\*]{1}\\s\\d+", str) ||
+                        Pattern.matches("^[IVXLCDM]+\\s[\\+\\-\\/\\*]{1}\\s[IVXLCDM]+", str.toUpperCase())){
+                    System.out.println(str + "pattern");
+                } else System.out.println("Введите корректно арифметическую операцию или введите команду \"exit\" для завершения операций.");
             } catch (IOException e) {
                 System.out.println("Введена пустая строка!");
+
                 e.printStackTrace();
                 break;
             } // end try
-        } // end while
 
+        } // end while
+        try {
+            reader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     } // end main
+
+
 }
